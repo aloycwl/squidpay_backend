027dccc200793911fcaf6a1fd4ec8d1f306777b9
Remix Workspace <>
2022-09-17 11:07:02 +0800

    Done

--- main.sol
+++ main.sol
@@ -1,8 +1,9 @@
 pragma solidity>0.8.0;//SPDX-License-Identifier:None
 contract Main{
 
+    uint private _count;
     address private _owner;
-    mapping(uint=>uint)public inventory;
+    mapping(uint=>uint)private inventory;
 
     constructor(){
         /*
@@ -17,9 +18,20 @@ contract Main{
         /*
         Loop through the entire array
         Only update when there is no similarity
+        Set count to always be the highest item number
         */
-        for(uint i;i<item.length;i++)
-            if(inventory[item[i]]!=count[i])
-                inventory[item[i]]=count[i];
+        for(uint i;i<item.length;i++){
+            if(inventory[item[i]]!=count[i])inventory[item[i]]=count[i];
+            if(item[i]>=_count)_count=item[i]+1;
+        }
+    }
+
+    function display()external view returns(uint[]memory count){
+        /*
+        Convert all items into json format
+        To input pagination?
+        */
+        count=new uint[](_count);
+        for(uint i;i<_count;i++)count[i]=inventory[i];
     }
 }
